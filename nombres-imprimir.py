2d67c7aeb462920820f1e5d6a1ca266686401958
FerLLanos <fer.llanos@duocuc.cl>
2024-07-14 22:12:38 +0000

    Change Cisco to NetAcad

---
+++ nombres-imprimir.py
@@ -0,0 +1,6 @@
+# Definir la lista de nombres y apellidos (lista manual)
+miembros_grupo = ["Fernando Llanos", "Guillermo Guajardo"]
+
+# Recorrer la lista y imprimir los nombres y apellidos
+for miembro in miembros_grupo:
+  print(miembro)
